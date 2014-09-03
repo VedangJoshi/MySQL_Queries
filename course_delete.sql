4e5ee9a983ebd943cfdafcc8cbb6049b6524c2d6
Vedang Joshi <joshi.vedang94@gmail.com>
2014-09-04 01:23:05 +0530

    Update course_delete.sql

--- course_delete.sql
+++ course_delete.sql
@@ -5,7 +5,7 @@ on     room
 for    each row 
 
 begin
-      insert into course_deletion_log values( concat(concat('Course Name :',old.course_name),                                                                                     concat('Course_number :',old.course_no))
+      insert into course_deletion_log values( concat(concat('Course Name :',old.course_name),concat('Course_number :',old.course_no))
                                               concat(concat('Department :',old.dept))
                                                      concat('Date of Deletion :',now)))                                                                  
         
